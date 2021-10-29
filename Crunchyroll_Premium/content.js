5e9155ee53282be983169f31643402a90e3b901b
JesusCrack23YT <93396406+JesusCrack23YT@users.noreply.github.com>
2021-10-29 12:23:58 -0400

    Add files via upload

--- Crunchyroll_Premium/content.js
+++ Crunchyroll_Premium/content.js
@@ -21,7 +21,7 @@ function importPlayer(){
     	console.log("[CR Premium] Adicionando o jwplayer...");
     	ifrm = document.createElement("iframe");
     	ifrm.setAttribute("id", "frame"); 
-		ifrm.setAttribute("src", "https://github.com/JesusCrack23YT/PrivateCR/"); 
+		ifrm.setAttribute("src", "https://mateus7g.github.io/crp-iframe-player/"); 
 		ifrm.setAttribute("width","100%");
 		ifrm.setAttribute("height","100%");
 		ifrm.setAttribute("frameborder","0");
